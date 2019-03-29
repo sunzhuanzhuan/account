aba10dd1bba597e11766f2258e0d9bc7eb07f7c6
wangxinyue <wangxinyue@weiboyi.com>
2019-03-29 14:13:50 +0800

    调整不给

--- src/accountDetail/components/LookIndex.js
+++ src/accountDetail/components/LookIndex.js
@@ -4,17 +4,17 @@ import { Icon } from 'antd';
 class LookIndex extends Component {
   state = {}
   render() {
-    const { qrCodeUrl, url } = this.props
+    const { qrCodeUrl, url, platformName } = this.props
     return (
       <PopoverFormat popoverProps={{ overlayStyle: { width: 150 } }}
         text={<div style={{ marginLeft: 10, }}><a style={{ color: ' #1990FF' }}><Icon type="eye" style={{ padding: '0px 3px' }} />查看主页</a></div>}
         content={
           <div >
-            <a style={{ color: ' #1990FF' }} href={url}>点击进入主页</a>
+            <a style={{ color: ' #1990FF' }} href={url} target='_blank'>点击进入主页</a>
             <div style={{ margin: '10px auto', width: 105, height: 105 }}>
               <img width='105' height='105' src={qrCodeUrl} />
             </div>
-            <div>打开抖音扫一扫，立即查看账号主页</div>
+            <div>打开{platformName}扫一扫，立即查看账号主页</div>
           </div>
         } />
     );
