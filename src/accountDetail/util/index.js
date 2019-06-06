0ca05aedce9d535a2cec482c640e698f4cf932ac
wangxinyue <wangxinyue@weiboyi.com>
2019-06-06 09:55:57 +0800

    调整一下

--- src/accountDetail/util/index.js
+++ src/accountDetail/util/index.js
@@ -60,6 +60,7 @@ export const getWeixinAvg = (value) => {
   }
 }
 export const getOtherAllAvg = (value) => {
+  console.log("TCL: getOtherAllAvg -> value", value)
   if (value) {
     let unit = ""
     let valueTransform = value || 0
