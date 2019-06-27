4c6f8c2f20729a4535053b8c45f12c1a371b1d8d
lzb <lzbabd@gmail.com>
2019-06-27 18:22:06 +0800

    展示bug修复

--- src/accountManage/components/common/CooperationCasesView.js
+++ src/accountManage/components/common/CooperationCasesView.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CooperationCasesView = ({ list = [] }) => {
   return list.length > 0 ? <div className='gray-block-container'>
     {
-      list.map((item, index) => <section key={item.cooperationCaseId} className='gray-block-item'>
+      list.map((item, index) => <section key={item.sort} className='gray-block-item'>
           <strong>案例{index + 1}</strong>
           <p>合作品牌： {item.brand}</p>
           <p>合作链接： <a target="_blank" href={item.link} className='url'>{item.link}</a></p>
