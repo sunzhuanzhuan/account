3998c32436bc55c0ec2b98cd2e774f5a2f22874c
lzb <lzbabd@gmail.com>
2019-07-08 15:12:17 +0800

    添加正常id跳转

--- src/accountManage/containers/UpdatePageCommonContainer.js
+++ src/accountManage/containers/UpdatePageCommonContainer.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react"
+
 export default class UpdatePageCommonContainer extends Component {
-	render() {
+  render() {
     const { data: { priceInfo } } = this.props.params;
     const {
-      policyInfoId,
+      policyInfoId
     } = priceInfo;
-		return <div className='update-page-container'>
-			<h2>
+    return <div className='update-page-container'>
+      <h2>
         账号维护
-        {policyInfoId ? <small className='policyInfo-id-display'>价格政策ID: <a href="#" >{policyInfoId}</a></small> : null}
-			</h2>
-			{this.props.children}
-		</div>
-	}
+        {policyInfoId ?
+          <small className='policyInfo-id-display'>
+            价格政策ID: <a href={"/account/policy?id=" + policyInfoId}>{policyInfoId}</a></small>
+          : null}
+      </h2>
+      {this.props.children}
+    </div>
+  }
 }
 
