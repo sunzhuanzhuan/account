b9e3b80319a29c8a169c7e0f60f5082f38a2f434
wangxinyue <wangxinyue@weiboyi>
2019-12-04 17:55:17 +0800

    添加数据指标配置

---
+++ src/accountDetail/components/delivery/Prediction.js
@@ -0,0 +1,78 @@
+
+import React, { useState } from 'react'
+import { Button, Form, Input, Select, Table } from 'antd';
+const Prediction = (props) => {
+  const [preResult, setPreResult] = useState(0)
+  const { getFieldDecorator, validateFields } = props.form
+  function startPrediction() {
+    validateFields((err, values) => {
+      if (!err) {
+        setPreResult(<PredicResult />)
+      }
+    });
+  }
+  return <Form layout='inline'>
+    <Form.Item label="请输入你投放的品牌名称">
+      {getFieldDecorator('bandName')(<Input />)}
+    </Form.Item>
+    <Form.Item label="品牌所属行业">
+      {getFieldDecorator('industry', {
+        initialValue: '0',
+      })(<Select>
+        {[{ name: '不限', id: '0' }].map(
+          one => <Select.Option
+            key={one.id} labelInValue={true}
+          >
+            {one.name}
+          </Select.Option>)}
+      </Select>)}
+    </Form.Item>
+    <Form.Item>
+      <Button type='primary' onClick={startPrediction}>开始预测</Button>
+    </Form.Item>
+    {preResult ? <div>
+      <h1>预测结果</h1>
+      {preResult}
+    </div> : null}
+  </Form>
+}
+const PredictionForm = Form.create()(Prediction)
+
+export default PredictionForm
+
+const PredicResult = () => {
+  const dataSource = [
+    {
+      key: '1',
+      name: '雅诗兰黛',
+      age: 32,
+      address: '播放量：8989',
+    },
+  ];
+
+  const columns = [
+    {
+      title: '品牌名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '成交价格',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '投放数据表现',
+      dataIndex: 'address',
+      key: 'address',
+    }, {
+      title: '投放效果',
+      dataIndex: 'address1',
+      key: 'address1',
+    }
+  ];
+
+  return <div>
+    <Table columns={columns} dataSource={dataSource} pagination={false} />
+  </div>
+}
---
+++ src/accountDetail/constants/DataIndicatorConfig.js
@@ -0,0 +1,98 @@
+export const getWechat = (data = {}) => {
+  return [
+    { name: "总发布数（原创文章数）", value: '382', unit: `个（${143}篇）` },
+    { name: "10w+阅读文章数", value: '8', unit: `个` },
+    { name: "真实阅读率", value: '48.5', unit: `%` },
+    { name: "近28天推送次数", value: '6', unit: `次` },
+    { name: "近28天发布频率", value: '2.9', unit: `个/周` },
+  ]
+}
+export const wechatColumns = [
+  {
+    title: '',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '平均阅读量',
+    dataIndex: 'avgread',
+    key: 'avgread',
+  },
+  {
+    title: '平均点赞数',
+    dataIndex: 'avglike',
+    key: 'avglike',
+  },
+  {
+    title: '最高点赞数',
+    dataIndex: 'highlike',
+    key: 'highlike',
+  },
+  {
+    title: '10w+阅读文章数',
+    dataIndex: 'read',
+    key: 'read',
+  },
+]
+export const getSina = (data = {}) => {
+  return [
+    { name: "粉丝数", value: '382', unit: `万` },
+    { name: "真粉率", value: '382', unit: `%` },
+    { name: "博文总数（原创 | 转发）", value: '382', unit: `个（${142} | ${240}）` },
+    { name: "图文微博数", value: '382', unit: `个` },
+    { name: "视频微博数", value: '382', unit: `个` },
+  ]
+}
+export const sinaColumns = [
+  {
+    title: '',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '平均评论数 | 最高转发数',
+    dataIndex: 'avgread',
+    key: 'avgread',
+  },
+]
+export const getRedBook = (data = {}) => {
+  return [
+    { name: "粉丝数", value: '382', unit: `万` },
+    { name: "真粉率", value: '45.2', unit: `%` },
+    { name: "笔记总数", value: '382', unit: `个` },
+    { name: "图文笔记", value: '382', unit: `个` },
+    { name: "视频笔记", value: '382', unit: `个` }
+  ]
+}
+export const redBookColumns = [
+  {
+    title: '',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '平均播放数',
+    dataIndex: 'avgread平均播放数',
+    key: 'avgread平均播放数',
+  },
+  {
+    title: '平均收藏数',
+    dataIndex: 'avgread平均收藏数',
+    key: 'avgread平均收藏数',
+  },
+  {
+    title: '平均转发数',
+    dataIndex: 'avgread平均转发数',
+    key: 'avgread平均转发数',
+  },
+  {
+    title: '平均点赞数',
+    dataIndex: 'avgread平均点赞数',
+    key: 'avgread平均点赞数',
+  },
+  {
+    title: '平均评论数',
+    dataIndex: 'avgread平均评论数',
+    key: 'avgread平均评论数',
+  },
+]
