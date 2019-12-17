e2d126cb96575d8f15a07dd90d6d7362607b01bf
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2019-12-17 15:56:28 +0800

    修改税率为发票税率

--- src/accountManage/components/common/AccountPrice.js
+++ src/accountManage/components/common/AccountPrice.js
@@ -549,7 +549,7 @@ class PriceTable extends Component {
     return <div>
       {partnerType ? <span>{partnerType == 1 ? '报价含税（' : '报价不含税'}
         {partnerType == 1 ? invoiceType == 1 ? '回票类型：增值税专用发票' : '回票类型：增值税普通发票）' : null}
-        {partnerType == 1 && invoiceType == 1 ? '，税点:' + taxRate * 100 + '%)' : null}
+        {partnerType == 1 && invoiceType == 1 ? '，发票税率：' + taxRate * 100 + '%)' : null}
       </span> : null}
       <div className='price-table'>
         <div className='price-table-head'>
