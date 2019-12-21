e6e5553cd39956dec96bd84228a337235ff3e177
wangxinyue <wangxinyue@weiboyi>
2019-12-21 10:50:29 +0800

    调整

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,6 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
-  app.use(proxy('/api/operator-gateway/accountDetail/v1/getBaseInfo',
-    { target: 'http://nb.pre-weiboyi.com', changeOrigin: true }
-  ));
-  app.use(proxy('/api/operator-gateway/',
-    { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  ));
-  app.use(proxy('/api/export/account/getAccountDetailDegreeList',
-    { target: 'http://172.16.121.123:7001/', changeOrigin: true }
-  ));
   app.use(proxy('/api',
-    { target: 'http://nb.pre-weiboyi.com', changeOrigin: true }
+    { target: 'http://nb.tst-weiboyi.com', changeOrigin: true }
   ));
 }
