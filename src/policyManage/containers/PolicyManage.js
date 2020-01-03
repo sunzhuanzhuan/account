9887473bb026230c0dd3231565fecd920b10336f
wangxinyue <wangxinyue@weiboyi>
2020-01-03 16:03:54 +0800

    调整上传值

--- src/policyManage/containers/PolicyManage.js
+++ src/policyManage/containers/PolicyManage.js
@@ -109,23 +109,17 @@ class PolicyManage extends React.Component {
       values.policyPeriodIdentity = policyPeriodIdentity;
       values.mcnId = mcnId;
       values.id = id;
-      if (values.contractFileUrl) {
-        const item = values.contractFile[0]
-        values.contractFileUrl = JSON.stringify([{
-          uid: '-1',
-          name: item.name,
-          status: 'done',
-          url: item.url,
-        }])
-      }
-      const _values = Object.keys(values).reduce((acc, cur) => {
-        if (values[cur]) {
-          acc[cur] = values[cur]
-          return acc;
-        }
-        return acc;
-      }, {})
-      this.props.saveProcurementPolicyInfo(_values)
+      const item = values.contractFile[0] || {}
+      values.contractFileUrl = item.url || ''
+      values.contractFileName = item.name || ''
+      // const _values = Object.keys(values).reduce((acc, cur) => {
+      //   if (values[cur]) {
+      //     acc[cur] = values[cur]
+      //     return acc;
+      //   }
+      //   return acc;
+      // }, {})
+      this.props.saveProcurementPolicyInfo(values)
     })
   }
 
@@ -380,8 +374,13 @@ class PolicyManage extends React.Component {
               {getFieldDecorator('contractFile', {
                 valuePropName: 'fileList',
                 getValueFromEvent: e => e && e.fileList,
-                initialValue: policyInfo.contractFileUrl && JSON.parse(policyInfo.contractFileUrl)
-
+                initialValue: policyInfo.contractFileUrl ?
+                  [{
+                    uid: '-1',
+                    name: policyInfo.contractFileName,
+                    status: 'done',
+                    url: policyInfo.contractFileUrl,
+                  }] : null
                 // rules: [
                 //    { message: '请上传截图', required: true, type: "array" }
                 // ]
