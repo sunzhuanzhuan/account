4077565de9c4fb90e09dbbc6eae17c169451a770
wangxinyue <wangxinyue@weiboyi>
2020-01-04 17:43:46 +0800

    添加规则清空提示语

--- src/policyManage/components/RuleModules/Account.js
+++ src/policyManage/components/RuleModules/Account.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Table, Input, Form, Select, Button, Icon } from 'antd'
+import { Table, Modal, Form, Select, Button, Icon, message } from 'antd'
+const { confirm } = Modal
 import AccountListTable from '../AccountListTable'
 // import {
 //     ruleDiscount,
@@ -37,8 +38,21 @@ export const AccountEdit = (props) => {
 export const AccountView = (props) => {
   const { accountList = [], delWhiteListAccount, cleanWhiteListAccount, isEdit } = props;
   const { labelName = '账号' } = props;
+  function showConfirm() {
+    confirm({
+      title: '是否清空所有账号',
+      onOk() {
+        //message.success('清空账号成功')
+        cleanWhiteListAccount()
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+
   return <Form.Item label={labelName} {...formItemLayout}>
-    <p>已选择{accountList.length}个账号 {isEdit ? <a onClick={cleanWhiteListAccount} style={{ float: "right" }}><Icon type="rest" />清空</a> : null}</p>
+    <p>已选择{accountList.length}个账号 {isEdit ? <a onClick={showConfirm} style={{ float: "right" }}><Icon type="rest" />清空</a> : null}</p>
     <AccountListTable isEdit={isEdit} delWhiteListAccount={delWhiteListAccount} dataSource={accountList} />
   </Form.Item>
 }
